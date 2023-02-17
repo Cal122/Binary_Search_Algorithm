a7f6506821c265faece8b04a3db01bbaa5025e1b
Cal122 <callumwork1122@gmail.com>
2023-02-17 11:40:40 -0800

    Restarted

--- main.py
+++
@@ -1,8 +0,0 @@
-def binary_search(list, element):
-    start = 0
-    middle = 0
-    end = len(list)
-    steps = 0
-
-    while(start <= end):
-        print(f"Step {steps} : {list[start, end+1]}")
